cc4d9a0d558d7b879f8cc489b4853085b95aec59
Oscar Johanneson <oscar@Oscars-MacBook-Pro.local>
2020-10-21 15:22:38 +0200

    first setup

---
+++ src/main/java/com/example/webservicelibrary/WebServiceLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.webservicelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webservicelibrary/WebServiceLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webservicelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
