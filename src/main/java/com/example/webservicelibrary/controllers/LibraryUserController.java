1baddb50b290dad2a249f2aa2ded4490af076953
Oscar Johanneson <oscar@Oscars-MacBook-Pro.local>
2020-10-21 22:22:06 +0200

    Added CRUD func to LibraryUser

---
+++ src/main/java/com/example/webservicelibrary/controllers/LibraryUserController.java
@@ -0,0 +1,48 @@
+package com.example.webservicelibrary.controllers;
+
+import com.example.webservicelibrary.enteties.LibraryUser;
+import com.example.webservicelibrary.services.LibraryUserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController //  REST API Endpoints.
+@RequestMapping("/library-api/v1/users")
+@Slf4j
+public class LibraryUserController {
+
+    @Autowired
+    private LibraryUserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<LibraryUser>> findAllLibraryUsers(@RequestParam(required = false) String name) {
+        // use name to later add functionality to search on user name.
+        return ResponseEntity.ok(userService.findAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LibraryUser> findLibraryUserById(@PathVariable String id) {
+        return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<LibraryUser> saveLibraryUser(@RequestBody LibraryUser user) {
+        return ResponseEntity.ok(userService.saveUser(user));
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateLibraryUser(@PathVariable String id, @RequestBody LibraryUser user) {
+        userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteLibraryUser(@PathVariable String id) {
+        userService.deleteUser(id);
+    }
+}
--- src/main/java/com/example/webservicelibrary/services/LibraryUserService.java
+++ src/main/java/com/example/webservicelibrary/services/LibraryUserService.java
@@ -4,19 +4,55 @@ import com.example.webservicelibrary.enteties.LibraryUser;
 import com.example.webservicelibrary.repositories.LibraryUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 @Slf4j
-@RequiredArgsConstructor
+@RequiredArgsConstructor // instead of @Autowired on LibraryUserRepository, make LibraryUserRepository final..
 public class LibraryUserService {
 
     private final LibraryUserRepository userRepository;
 
     public List<LibraryUser> findAllUsers() {
+        log.info("Request to find all users.");
+
         var users = userRepository.findAll();
         return users;
     }
+
+    public LibraryUser findById(String id) {
+        if(!userRepository.existsById(id)) log.error(String.format("User with this id %s. , could not be found", id));
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        String.format("User with this id %s. , could not be found", id)));
+    }
+
+    public LibraryUser saveUser(LibraryUser user) {
+        return userRepository.save(user);
+    }
+
+    public void updateUser(String id, LibraryUser user) {
+        if(!userRepository.existsById(id)){
+            log.error(String.format("User with this id %s. , could not be found", id));
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    String.format("User with this id %s. , could not be found", id));
+        }
+
+        user.setId(id);
+        userRepository.save(user);
+    }
+
+    public void deleteUser(String id) {
+        if(!userRepository.existsById(id)) {
+            log.error(String.format("User with this id %s. , could not be found", id));
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    String.format("User with this id %s. , could not be found", id));
+        }
+        userRepository.deleteById(id);
+    }
+
 }
